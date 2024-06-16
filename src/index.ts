f6dff4d348b4869f8ca31f5e4a5ce877d841d08d
Andrew <66942330+Noro95@users.noreply.github.com>
2024-06-16 23:18:35 +0300

    update: remove express-status-monitor due to dep and vuln subdependencies

--- src/index.ts
+++ src/index.ts
@@ -23,14 +23,14 @@ import path from "node:path";
 import { connect } from "mongoose";
 import { FindUser } from "./database/functions/user.js";
 import { Octokit } from "@octokit/rest";
-import StatusMonitor from "express-status-monitor";
+// import StatusMonitor from "express-status-monitor";
 
 // routers
 import github from "./routers/github.js";
 
 const app: Application = express();
 
-app.use(StatusMonitor());
+// app.use(StatusMonitor());
 
 cacheCommands(commandsData);
 
