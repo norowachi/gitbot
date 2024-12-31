6f3ffe1c25b19de7bb51d364d00cdf250f286c5a
Noro@CV2 <66942330+Noro95@users.noreply.github.com>
2024-12-31 05:19:41 +0100

    refactor: revert interaction endpoint `3e27a46` & remove need for pubkey in env

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ import {
 	APIInteraction,
 	APIChatInputApplicationCommandInteraction,
 	Routes,
+	RESTGetCurrentApplicationResult,
 } from "discord-api-types/v10";
 import fs from "node:fs";
 import path from "node:path";
@@ -47,8 +48,8 @@ rest.req("PATCH", Routes.currentApplication(), {
 				.slice(0, 7)}\``,
 			``,
 			`Integration of the Discord and GitHub API, so you can submit your bugs (and memleaks!) without stopping your conversation :D`,
-			`Terms: https://noro.cc/gitbot/terms`,
-			`Privacy Policy: https://noro.cc/gitbot/privacy`,
+			`**Terms**: https://noro.cc/gitbot/terms`,
+			`**Privacy Policy**: https://noro.cc/gitbot/privacy`,
 			``,
 			`Not affiliated with either Discord nor GitHub.`,
 			`Disclaimer; avatar & banner belong to https://jasonlong.me`,
@@ -56,141 +57,141 @@ rest.req("PATCH", Routes.currentApplication(), {
 	},
 });
 
+const PubKey = (
+	(await rest.req(
+		"GET",
+		Routes.currentApplication()
+	)) as RESTGetCurrentApplicationResult
+).verify_key;
+
 // using github router
 app.use("/github", github);
 
 // the interactions endpoint
-app.post(
-	"*/interactions",
-	verifyKeyMiddleware(env.DISCORD_APP_PUBLIC_KEY!),
-	async (req, res) => {
-		let interaction: APIInteraction = req.body;
+app.post("/interactions", verifyKeyMiddleware(PubKey), async (req, res) => {
+	let interaction: APIInteraction = req.body;
 
-		// if its an autocomplete interaction
-		if (
-			interaction.type === InteractionType.ApplicationCommandAutocomplete
-		) {
-			// get DBUser
-			const DBUser = await getUser({
-				discordId: interaction.user?.id || interaction.member?.user.id,
-			});
+	// if its an autocomplete interaction
+	if (interaction.type === InteractionType.ApplicationCommandAutocomplete) {
+		// get DBUser
+		const DBUser = await getUser({
+			discordId: interaction.user?.id || interaction.member?.user.id,
+		});
 
-			// if user isnt auth'd then ignore
-			if (!(DBUser && DBUser.github.access_token)) return;
+		// if user isnt auth'd then ignore
+		if (!(DBUser && DBUser.github.access_token)) return;
 
-			// get the focused field
-			const focused = getFocusedField(interaction.data.options!);
+		// get the focused field
+		const focused = getFocusedField(interaction.data.options!);
 
-			// if focused field is not found then ignore
-			if (!focused) return;
+		// if focused field is not found then ignore
+		if (!focused) return;
 
-			// run command's autocomplete
-			await (
-				await runCommandAutoComplete(interaction.data.name!)
-			)(
-				res,
-				focused,
-				[
-					DBUser,
-					new Octokit({
-						auth: decryptToken(DBUser.github.access_token),
-						userAgent: "gitbot.norowa.dev",
-					}),
-				],
-				interaction.data?.options
-					? getOptionsValue(interaction.data?.options)
-					: undefined
-			);
-			return;
-		}
-
-		console.log(
-			DateInISO(),
-			ChangeConsoleColor(
-				"FgRed",
-				interaction.user?.username ||
-					interaction.member?.user.username ||
-					"Unknown"
-			),
-			"used",
-			ChangeConsoleColor(
-				"FgCyan",
-				(interaction.data as any)?.name,
-				interaction.type
-			),
-			"in",
-			ChangeConsoleColor(
-				"FgMagenta",
-				interaction.channel?.name,
-				"|",
-				interaction.channel?.id
-			)
-		);
-
-		// if its a message component or a modal submit emit an interaction with given custom id
-		if (
-			interaction.type === InteractionType.MessageComponent ||
-			interaction.type === InteractionType.ModalSubmit
-		) {
-			IntEmitter.emit(interaction.data.custom_id, res, interaction);
-			return;
-		}
-
-		// respond to a ping from discord
-		if (interaction.type === InteractionType.Ping) {
-			return res.json({
-				type: InteractionResponseType.Pong,
-			});
-		}
-
-		// if its an application command
-		if (interaction.type === InteractionType.ApplicationCommand) {
-			interaction =
-				interaction as APIChatInputApplicationCommandInteraction;
-
-			// get DBUser
-			const DBUser = await getUser({
-				discordId: interaction.user?.id || interaction.member?.user.id,
-			});
-
-			// if command is not `link` and user is not in db, say nuh uh
-			if (interaction.data.name !== "link" && !DBUser) {
-				return res.json({
-					type: InteractionResponseType.ChannelMessageWithSource,
-					data: {
-						content:
-							"You have to link your github account first, please use the `/link` command.",
-						flags: MessageFlags.Ephemeral,
-					},
-				});
-			}
-
-			// initialize octokit
-			let octokit: null | Octokit = null;
-
-			// if db user and access token exists set octokit
-			if (DBUser && DBUser.github.access_token) {
-				octokit = new Octokit({
-					auth: decryptToken(DBUser!.github.access_token),
+		// run command's autocomplete
+		await (
+			await runCommandAutoComplete(interaction.data.name!)
+		)(
+			res,
+			focused,
+			[
+				DBUser,
+				new Octokit({
+					auth: decryptToken(DBUser.github.access_token),
 					userAgent: "gitbot.norowa.dev",
-				});
-			}
-
-			// run command
-			await (
-				await runCommand(interaction.data.name!)
-			)(
-				res,
-				octokit ? [DBUser!, octokit] : [],
-				getSub(interaction.data?.options?.at(0)),
-				interaction.data?.options
-					? getOptionsValue(interaction.data?.options)
-					: undefined
-			);
-			return;
-		} else return;
+				}),
+			],
+			interaction.data?.options
+				? getOptionsValue(interaction.data?.options)
+				: undefined
+		);
+		return;
 	}
-);
+
+	console.log(
+		DateInISO(),
+		ChangeConsoleColor(
+			"FgRed",
+			interaction.user?.username ||
+				interaction.member?.user.username ||
+				"Unknown"
+		),
+		"used",
+		ChangeConsoleColor(
+			"FgCyan",
+			(interaction.data as any)?.name,
+			interaction.type
+		),
+		"in",
+		ChangeConsoleColor(
+			"FgMagenta",
+			interaction.channel?.name,
+			"|",
+			interaction.channel?.id
+		)
+	);
+
+	// if its a message component or a modal submit emit an interaction with given custom id
+	if (
+		interaction.type === InteractionType.MessageComponent ||
+		interaction.type === InteractionType.ModalSubmit
+	) {
+		IntEmitter.emit(interaction.data.custom_id, res, interaction);
+		return;
+	}
+
+	// respond to a ping from discord
+	if (interaction.type === InteractionType.Ping) {
+		return res.json({
+			type: InteractionResponseType.Pong,
+		});
+	}
+
+	// if its an application command
+	if (interaction.type === InteractionType.ApplicationCommand) {
+		interaction = interaction as APIChatInputApplicationCommandInteraction;
+
+		// get DBUser
+		const DBUser = await getUser({
+			discordId: interaction.user?.id || interaction.member?.user.id,
+		});
+
+		// if command is not `link` and user is not in db, say nuh uh
+		if (interaction.data.name !== "link" && !DBUser) {
+			return res.json({
+				type: InteractionResponseType.ChannelMessageWithSource,
+				data: {
+					content:
+						"You have to link your github account first, please use the `/link` command.",
+					flags: MessageFlags.Ephemeral,
+				},
+			});
+		}
+
+		// initialize octokit
+		let octokit: null | Octokit = null;
+
+		// if db user and access token exists set octokit
+		if (DBUser && DBUser.github.access_token) {
+			octokit = new Octokit({
+				auth: decryptToken(DBUser!.github.access_token),
+				userAgent: "gitbot.norowa.dev",
+			});
+		}
+
+		// run command
+		await (
+			await runCommand(interaction.data.name!)
+		)(
+			res,
+			octokit ? [DBUser!, octokit] : [],
+			getSub(interaction.data?.options?.at(0)),
+			interaction.data?.options
+				? getOptionsValue(interaction.data?.options)
+				: undefined
+		);
+		return;
+	} else return;
+});
 
 app.listen(env.PORT, async () => {
 	connect(env.MONGO_URI!)
